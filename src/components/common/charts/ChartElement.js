7f79fab9bae12b68e05e3004dc6382a19397cd93
George Sigoiu <georgesigoiu1@gmail.com>
2022-11-03 18:39:06 +0200

    charts finished

--- src/components/common/charts/ChartElement.js
+++ src/components/common/charts/ChartElement.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MyBarChart from './MyBarChart'
 import MyLineChart from './MyLineChart'
 import './chart_element.css'
-const ChartElement = ({ device }) => {
+import MyDatePicker from './MyDatePicker';
+import { getRequest, LINK_GET_CONSUMPTION_FOR_DATE } from '../../requests';
+import { requestHandler } from '../../handlers'
+
+const ChartElement = ({ device, tokens, setTokens }) => {
+
+    const [selectedDate, setSelectedDate] = useState(new Date().toISOString())
+    const [data, setData] = useState([])
+    const [totalEnergyConsumption, setTotalEnergyConsumption] = useState(0)
+
+    function constructData(data) {
+        const list = []
+        let energy = 0
+        for (let i = 0; i < 24; i++) {
+            const el = {
+                time: i + ":00",
+                value: data[i]
+            }
+            energy = Number(energy) + Number(data[i])
+            list.push(el)
+        }
+        setData(list)
+        setTotalEnergyConsumption(energy)
+    }
+
+    useEffect(() => {
+    }, [data])
+
+    async function getConsumption(date) {
+        const response = await requestHandler(getRequest, {
+            link: LINK_GET_CONSUMPTION_FOR_DATE.replace("DATE", date.toString()).replace("DEVICEID", device.id),
+            payload: {}
+        }, tokens, setTokens)
+        constructData(response)
+    }
+
+    useEffect(() => {
+        //todo get the values for this date
+        getConsumption(selectedDate)
+    }, [selectedDate])
+
 
     function click(e) {
         const target = e.currentTarget.children[0]
@@ -24,25 +64,34 @@ const ChartElement = ({ device }) => {
 
     return (
         <div id="charts-elements">
-            <ul >
-                <li onClick={e => click(e)}>
-                    <a data-toggle="tab" id={`ref-line-chart-${device.id}`}>
-                        Line chart
-                    </a>
-                </li>
-                <li onClick={e => click(e)}>
-                    <a data-toggle="tab" id={`ref-bar-chart-${device.id}`}>
-                        Bar chart
-                    </a>
-                </li>
-            </ul>
-            <div className='tab-content'>
+            <div style={{ display: "flex", justifyContent: "space-between" }}>
+                <ul>
+                    <li onClick={e => click(e)}>
+                        <a data-toggle="tab" id={`ref-line-chart-${device.id}`}>
+                            Line chart
+                        </a>
+                    </li>
+                    <li onClick={e => click(e)}>
+                        <a data-toggle="tab" id={`ref-bar-chart-${device.id}`}>
+                            Bar chart
+                        </a>
+                    </li>
+                </ul>
+                <div style={{ width: "fit-content" }}>
+                    <MyDatePicker setSelectedDate={setSelectedDate} selectedDate={selectedDate} />
+                </div>
+            </div>
+
+            <div className='tab-content' style={{ marginLeft: "-2rem" }}>
                 <div id={`line-chart-${device.id}`} className='tab-pane fade active show'>
-                    <MyLineChart device={device} />
+                    <MyLineChart data={data} />
                 </div>
                 <div id={`bar-chart-${device.id}`} className='tab-pane fade'>
-                    <MyBarChart device={device} />
+                    <MyBarChart data={data} />
                 </div>
+            </div>
+            <div>
+                Total energy consumption: {totalEnergyConsumption} W
             </div>
         </div>
     )
--- src/components/common/charts/MyBarChart.js
+++ src/components/common/charts/MyBarChart.js
@@ -1,34 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
-const MyBarChart = ({ device }) => {
+const MyBarChart = ({ data }) => {
 
-    const data = [
-        { date: "23 Oct", time: "0:00", value: 2 },
-        { date: "23 Oct", time: "1:00", value: 2 },
-        { date: "23 Oct", time: "2:00", value: 2 },
-        { date: "23 Oct", time: "3:00", value: 2 },
-        { date: "23 Oct", time: "4:00", value: 2 },
-        { date: "23 Oct", time: "5:00", value: 2 },
-        { date: "23 Oct", time: "6:00", value: 2 },
-        { date: "23 Oct", time: "7:00", value: 3 },
-        { date: "23 Oct", time: "8:00", value: 1 },
-        { date: "23 Oct", time: "9:00", value: 2 },
-        { date: "23 Oct", time: "10:00", value: 10 },
-        { date: "23 Oct", time: "11:00", value: 5 },
-        { date: "23 Oct", time: "12:00", value: 25 },
-        { date: "23 Oct", time: "13:00", value: 6 },
-        { date: "23 Oct", time: "14:00", value: 10 },
-        { date: "23 Oct", time: "15:00", value: 4 },
-        { date: "23 Oct", time: "16:00", value: 20 },
-        { date: "23 Oct", time: "17:00", value: 11 },
-        { date: "23 Oct", time: "18:00", value: 9 },
-        { date: "23 Oct", time: "19:00", value: 9 },
-        { date: "23 Oct", time: "20:00", value: 9 },
-        { date: "23 Oct", time: "21:00", value: 9 },
-        { date: "23 Oct", time: "22:00", value: 9 },
-        { date: "23 Oct", time: "23:00", value: 9 }
-    ]
+
     return (
         <BarChart width={700} height={300} data={data}>
             <CartesianGrid strokeDasharray="3 3" />
@@ -36,7 +11,6 @@ const MyBarChart = ({ device }) => {
             <YAxis />
             <Tooltip />
             <Bar dataKey="value" fill="#8884d8" />
-            {/* <Bar dataKey="uv" fill="#82ca9d" /> */}
         </BarChart>
     )
 }
