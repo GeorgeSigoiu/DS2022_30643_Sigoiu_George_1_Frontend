4e2572a1c63e8d2c25f6dbe2fd86f5954d2d3a00
George Sigoiu <georgesigoiu1@gmail.com>
2022-12-20 18:21:10 +0200

    modify websocket for receiving messages from devices

--- src/components/common/WebSocket.js
+++ src/components/common/WebSocket.js
@@ -1,31 +1,32 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import SockJsClient from 'react-stomp'
 
-const SOCKET_URL = 'http://20.113.96.159:7900/ws-message';
-// const SOCKET_URL = 'http://localhost:7901/ws-message';
+// const SOCKET_URL = 'http://20.113.96.159:7900/ws-message';
+const SOCKET_URL = 'http://localhost:7901/ws-message';
 
+/**
+ * Used for receiving from backend the consumption for 
+ */
 const WebSocket = ({ setMessage, setNewConsumption, username }) => {
 
+
     let onConnected = () => {
-        console.log("Connected!!")
+        console.log("Connected for devices consumption!!")
+    }
+
+    let onDisconnect = () => {
+        console.log("Disconnect")
     }
 
     let onMessageReceived = (msg) => {
-        console.log("Got the message: ", msg)
         const content1 = msg.message_exceeded_consumption
         const content2 = msg.device_consumption
         setNewConsumption(content2)
         const obj = JSON.parse(content1)
         if (JSON.stringify(obj) !== "{}") {
-            console.log("add obj: ", obj)
             setMessage(obj);
         }
     }
-
-    useEffect(() => {
-        console.log("username", username)
-        console.log("username ls", localStorage.getItem("username"))
-    }, [username])
 
     return (
         <div>
@@ -34,9 +35,9 @@ const WebSocket = ({ setMessage, setNewConsumption, username }) => {
                 (
                     <SockJsClient
                         url={SOCKET_URL}
-                        topics={['/topic/message', "/user/" + localStorage.getItem("username") + "/private"]}
+                        topics={["/user/" + localStorage.getItem("username") + "/private"]}
                         onConnect={onConnected}
-                        onDisconnect={console.log("Disconnected!")}
+                        onDisconnect={onDisconnect}
                         onMessage={msg => onMessageReceived(msg)}
                     />
                 )
